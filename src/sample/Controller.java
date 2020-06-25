a2563b51a7aed99b437e1ff1cb2e1d444acb61f2
velkonini <avelkov07@gmail.com>
2020-06-25 20:08:54 +0300

    Первая версия окна

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,20 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    @FXML
+    public TextField textField;
+    @FXML
+    public TextArea textArea;
+
+    public void clickButton() {
+        textArea.appendText(textField.getText() + "\n");
+        textField.clear();
+        textField.requestFocus();
+
+    }
 }
